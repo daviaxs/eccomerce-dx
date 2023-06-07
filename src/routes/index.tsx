e80e43a190e6eaba187a7a3af4a7957b01010ddd
Davi Alves <cadwvk@gmail.com>
2023-06-07 17:13:09 -0300

    Created home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { Home } from '@/pages'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={} />
+      <Route path="/pagina-inicial" element={<Home />} />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   )
 }
