dcfb41007581be319ec26305a77810b12afe50d5
Davi Alves <cadwvk@gmail.com>
2023-06-08 11:56:39 -0300

    Added Heading Secondary in fonts

--- src/shared/fonts/Fonts.style.tsx
+++ src/shared/fonts/Fonts.style.tsx
@@ -24,6 +24,18 @@ const THeadingPrimaryStyle = styled.h1<ITextProps>`
   text-shadow: ${(props) => props.txtShadow};
 `
 
+const THeadingSecondaryStyle = styled.h2<ITextProps>`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  font-size: ${(props) => props.fontSize}rem;
+  white-space: ${(props) => props.whiteSpace};
+  text-align: ${(props) => props.textAlign};
+  line-height: 100%;
+
+  color: ${(props) => props.txtColor};
+  text-shadow: ${(props) => props.txtShadow};
+`
+
 const TTitlePrimaryStyle = styled.strong<ITextProps>`
   font-family: 'Roboto', sans-serif;
   font-weight: 500;
@@ -58,6 +70,31 @@ export const THeadingPrimary: React.FC<ITextProps> = ({
     >
       {children}
     </THeadingPrimaryStyle>
+  )
+}
+
+export const THeadingSecondary: React.FC<ITextProps> = ({
+  txtColor,
+  className,
+  txtShadow,
+  textAlign,
+  whiteSpace = 'normal',
+  fontSize = 2,
+  display,
+  children,
+}) => {
+  return (
+    <THeadingSecondaryStyle
+      txtColor={txtColor}
+      txtShadow={txtShadow}
+      textAlign={textAlign}
+      whiteSpace={whiteSpace}
+      fontSize={fontSize}
+      className={className}
+      display={display}
+    >
+      {children}
+    </THeadingSecondaryStyle>
   )
 }
 
