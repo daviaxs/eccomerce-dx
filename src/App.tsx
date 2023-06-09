56b6dbf8e1701e3a72fa39430b1f312b679db780
Davi Alves <cadwvk@gmail.com>
2023-06-09 20:49:17 -0300

    Imported CartMenuProvider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 
 import { AppRoutes } from '@/routes'
+import { CartMenuProvider } from './shared/contexts/CartMenuContext'
 import { WindowDimensionsProvider } from './shared/contexts/WindowDimensionsContext'
 
 export function App() {
   return (
     <WindowDimensionsProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <CartMenuProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </CartMenuProvider>
     </WindowDimensionsProvider>
   )
 }
