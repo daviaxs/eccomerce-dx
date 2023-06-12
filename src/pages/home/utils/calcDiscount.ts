a5ef2cc1cc0bf41c7ea9bf97b2be0bfd5b2317e2
Davi Alves <cadwvk@gmail.com>
2023-06-12 18:01:05 -0300

    Created func roundNumber

--- src/pages/home/utils/calcDiscount.ts
+++ src/pages/home/utils/calcDiscount.ts
@@ -2,3 +2,7 @@
 export function CalcDiscout(original_price: number, price: number): number {
   return ((original_price - price) / original_price) * 100
 }
+
+export function roundNumber(number: number) {
+  return Math.round(number)
+}
