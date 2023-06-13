0848b4538dc322e1fa8fbdf2db77ee451fb645f4
Davi Alves <cadwvk@gmail.com>
2023-06-13 17:27:16 -0300

    Created interface in options icon file

--- src/shared/assets/options.tsx
+++ src/shared/assets/options.tsx
@@ -1,15 +1,20 @@
-export function OptionsIcon() {
+interface IIconProps {
+  size: number
+  color: string
+}
+
+export function OptionsIcon({ color = 'white', size }: IIconProps) {
   return (
     <svg
-      width="35"
-      height="30"
+      width={size}
+      height={size}
       viewBox="0 0 35 30"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M2 1.5H33M2 15H33M2 28.5H33"
-        stroke="#BDBDBD"
+        stroke={color}
         strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
