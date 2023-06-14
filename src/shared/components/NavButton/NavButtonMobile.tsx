cf0634b2cc34ca0bd9448c2ae3993b6e3359ea9c
Davi Alves <cadwvk@gmail.com>
2023-06-14 17:40:05 -0300

    Added css base in NavButtonMobile

--- src/shared/components/NavButton/NavButtonMobile.tsx
+++ src/shared/components/NavButton/NavButtonMobile.tsx
@@ -2,7 +2,11 @@ import { theme } from '@/shared/theme'
 import styled from 'styled-components'
 
 const NavButtonMobileStyle = styled.button`
-  
+  display: flex;
+  align-items: center;
+  padding: 0.625rem 1rem;
+  gap: 1.25rem;
+  background-color: ${theme.gray[700]};
 `
 
 export function NavButtonMobile() {
