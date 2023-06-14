0d20166fa250dd71ec2a3af45f305deaa038be7f
Davi Alves <cadwvk@gmail.com>
2023-06-14 17:35:04 -0300

    created nav container in MenuNav component

--- src/shared/layout/utils/MenuNav.tsx
+++ src/shared/layout/utils/MenuNav.tsx
@@ -1,15 +1,30 @@
 import { theme } from '@/shared/theme'
 import styled from 'styled-components'
 
-const MenuNavStyle = styled.nav`
+const MenuNavStyle = styled.div`
   position: fixed;
   width: 100vw;
   height: 100vh;
   background-color: ${theme.gray[900]};
   margin-top: 4rem;
   z-index: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: start;
+`
+
+const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 1.5rem;
 `
 
 export function MenuNav() {
-  return <MenuNavStyle></MenuNavStyle>
+  return (
+    <MenuNavStyle>
+      <NavContainer></NavContainer>
+    </MenuNavStyle>
+  )
 }
