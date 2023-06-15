4053ebe200c3fa4ed6f8f2eefaa05c88e929ea82
Davi Alves <cadwvk@gmail.com>
2023-06-15 17:24:33 -0300

    updated CarMenu component

--- src/shared/components/cartMenu/CartMenu.tsx
+++ src/shared/components/cartMenu/CartMenu.tsx
@@ -1,3 +1,13 @@
+import styled from 'styled-components'
+
+const CartMenuBG = styled.div``
+
+const CartMenuStyle = styled.div``
+
 export function CartMenu() {
-  return <></>
+  return (
+    <CartMenuBG>
+      <CartMenuStyle></CartMenuStyle>
+    </CartMenuBG>
+  )
 }
