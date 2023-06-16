83cd1538a15e0668a4fa5c1c6cf44e5eee79f405
Davi Alves <cadwvk@gmail.com>
2023-06-16 12:29:24 -0300

    Added new funcs in CartMenu and updated CartMenuBG css

--- src/shared/components/cartMenu/CartMenu.tsx
+++ src/shared/components/cartMenu/CartMenu.tsx
@@ -1,10 +1,30 @@
+import { useCartMenuContext } from '@/shared/contexts/CartMenuContext'
+import { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-const CartMenuBG = styled.div``
+const CartMenuBG = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: #00000038;
+  z-index: 10;
+`
 
 const CartMenuStyle = styled.div``
 
 export function CartMenu() {
+  const { expandedCartMenu } = useCartMenuContext()
+  // eslint-disable-next-line prettier/prettier
+  const [shouldRenderCartMenu, setShouldRenderCartMenu] = useState(expandedCartMenu)
+
+  useEffect(() => {
+    expandedCartMenu ?? setShouldRenderCartMenu(true)
+  }, [expandedCartMenu])
+
+  const handleAnimationEnd = useCallback(() => {
+    !expandedCartMenu ?? setShouldRenderCartMenu(false)
+  }, [expandedCartMenu])
+
   return (
     <CartMenuBG>
       <CartMenuStyle></CartMenuStyle>
