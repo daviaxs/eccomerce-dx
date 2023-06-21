09b996dfcee0140729d9b358befee7ca3e2afb14
Davi Alves <cadwvk@gmail.com>
2023-06-21 15:45:03 -0300

    created const getItem

--- src/shared/services/LocalStorageFuncs.ts
+++ src/shared/services/LocalStorageFuncs.ts
@@ -6,3 +6,8 @@ interface ISetItem {
 export const setItem = ({ key, value }: ISetItem) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
+
+export const getItem = (key: any) => {
+  const value = localStorage.getItem(key)
+  return value ? JSON.parse(value) : null
+}
