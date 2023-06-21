81642c143040afd2eb33a42820d04707eefb1c18
Davi Alves <cadwvk@gmail.com>
2023-06-21 16:41:44 -0300

    added buttonClose in CartHeader component

--- src/shared/components/cartMenu/utils/CartHeader.tsx
+++ src/shared/components/cartMenu/utils/CartHeader.tsx
@@ -1,11 +1,13 @@
+import { useCartMenuContext } from '@/shared/contexts/CartMenuContext'
 import { TTextPrimary } from '@/shared/fonts/Fonts.style'
 import { theme } from '@/shared/theme'
+import { X } from 'lucide-react'
 import styled from 'styled-components'
 
 const CartHeaderStyle = styled.header`
   display: flex;
   align-items: center;
-  justify-content: start;
+  justify-content: space-between;
 
   width: 100%;
   height: 3.5rem;
@@ -15,14 +17,21 @@ const CartHeaderStyle = styled.header`
   padding: 1.5rem;
 
   position: fixed;
+
+  .closeButton {
+    cursor: pointer;
+  }
 `
 
 export function CartHeader() {
+  const { toggleCartMenu } = useCartMenuContext()
+
   return (
     <CartHeaderStyle>
       <TTextPrimary fontSize={1}>
         Seu carrinho tem <strong>4 itens!</strong>
       </TTextPrimary>
+      <X size={30} className="closeButton" onClick={toggleCartMenu} />
     </CartHeaderStyle>
   )
 }
