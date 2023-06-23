c5c147eca0a3cee8e2a7580b18ca21bdbc1127bf
Davi Alves <cadwvk@gmail.com>
2023-06-23 20:37:45 -0300

    Updated getItem type

--- src/shared/services/LocalStorageFuncs.ts
+++ src/shared/services/LocalStorageFuncs.ts
@@ -7,7 +7,7 @@ export const setItem = ({ key, value }: ISetItem) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
 
-export const getItem = (key: any) => {
+export const getItem = (key: string) => {
   const value = localStorage.getItem(key)
   return value ? JSON.parse(value) : null
 }
