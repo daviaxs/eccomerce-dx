0b5cde47fa4d0d298cf262d5eed15421a87bd8c2
Davi Alves <cadwvk@gmail.com>
2023-06-25 11:36:49 -0300

    Added css base in CartProduct

--- src/shared/components/cartMenu/utils/cartContent/CartProduct.tsx
+++ src/shared/components/cartMenu/utils/cartContent/CartProduct.tsx
@@ -1,12 +1,27 @@
+import { theme } from '@/shared/theme'
 import styled from 'styled-components'
 
-const ProductStyle = styled.li``
+const ProductStyle = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  padding: 1rem;
+  width: 100%;
+
+  border-bottom: 1px solid ${theme.gray[400]};
+
+  .productImage {
+    border-radius: 0.5rem;
+  }
+`
 
 export function CartProduct() {
   return (
     <ProductStyle>
       <img
         src="http://http2.mlstatic.com/D_865221-MLU69233686783_052023-I.jpg"
+        className="productImage"
         alt=""
       />
     </ProductStyle>
