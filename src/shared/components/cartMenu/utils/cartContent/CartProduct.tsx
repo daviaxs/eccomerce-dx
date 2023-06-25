9d7359fff0e5ed919b2ffe7fe3230fe6ec0a4563
Davi Alves <cadwvk@gmail.com>
2023-06-25 11:44:18 -0300

    Added product title in CartProduct

--- src/shared/components/cartMenu/utils/cartContent/CartProduct.tsx
+++ src/shared/components/cartMenu/utils/cartContent/CartProduct.tsx
@@ -1,12 +1,17 @@
-import { theme } from '@/shared/theme'
 import styled from 'styled-components'
+
+import { Container } from '@/shared/components/container/Container'
+import { THeadingPrimary } from '@/shared/fonts/Fonts.style'
+import { theme } from '@/shared/theme'
 
 const ProductStyle = styled.li`
   display: flex;
   align-items: center;
   justify-content: start;
 
+  gap: 1rem;
   padding: 1rem;
+  height: 7.5rem;
   width: 100%;
 
   border-bottom: 1px solid ${theme.gray[400]};
@@ -24,6 +29,17 @@ export function CartProduct() {
         className="productImage"
         alt=""
       />
+      <Container
+        display="flex"
+        align="start"
+        justifyContent="start"
+        width="100%"
+        height="100%"
+      >
+        <THeadingPrimary fontSize={0.8}>
+          Samsung Galaxy A14 Dual 128gb 4gb Ram Tela 6.6 4g Câm 50mp
+        </THeadingPrimary>
+      </Container>
     </ProductStyle>
   )
 }
