09d817f5c30ba2a325a6bb5ea0668581cdc31424
Davi Alves <cadwvk@gmail.com>
2023-06-25 12:21:47 -0300

    Added css base in ButtonAddAndRemove component

--- src/shared/components/cartMenu/utils/cartContent/ButtonAddAndRemove.tsx
+++ src/shared/components/cartMenu/utils/cartContent/ButtonAddAndRemove.tsx
@@ -1,3 +1,17 @@
+import styled from 'styled-components'
+
+const ButtonAddAndRemoveStyle = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: transparent;
+  border-radius: 0.5rem;
+
+  width: 1.3rem;
+  height: 1.3rem;
+`
+
 export function ButtonAddAndRemove() {
-  return <></>
+  return <ButtonAddAndRemoveStyle></ButtonAddAndRemoveStyle>
 }
