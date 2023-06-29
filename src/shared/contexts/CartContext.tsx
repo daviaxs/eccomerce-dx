f25c0b4b7279b9b5cc9e4c1d2b2de864fadac0e1
Davi Alves <cadwvk@gmail.com>
2023-06-29 19:24:25 -0300

    bug fix in addProduct func

--- src/shared/contexts/CartContext.tsx
+++ src/shared/contexts/CartContext.tsx
@@ -28,24 +28,19 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
 
   const [data, setData] = useState(shopCart ? JSON.parse(shopCart) : [])
   const [quantity, setQuantity] = useState(
-    quantityCart ? JSON.parse(quantityCart) : {},
+    quantityCart ? JSON.parse(quantityCart) : 1,
   )
 
   const addProduct = (product: IProductProps) => {
-    const element = data.find((e: IProductProps) => e.id === product.id)
+    const productInCart = data.find((e: IProductProps) => e.id === product.id)
 
-    if (element) {
-      const newData = data.find((e: IProductProps) => e.id !== product.id)
-      setData(newData)
-      setItem({ key: 'shopCart', value: newData })
-
-      const newQuantity = { ...quantity }
-      delete newQuantity[product.id]
+    if (productInCart) {
+      const newQuantity = {
+        ...quantity,
+        [product.id]: quantity[product.id] + 1,
+      }
       setQuantity(newQuantity)
       setItem({ key: 'quantity', value: newQuantity })
-
-      const event = new CustomEvent('cartChange', { detail: newData })
-      window.dispatchEvent(event)
     } else {
       setData([...data, product])
       setItem({ key: 'shopCart', value: [...data, product] })
@@ -60,6 +55,36 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
       window.dispatchEvent(event)
     }
   }
+
+  // const addProduct = (product: IProductProps) => {
+  //   const productInCart = data.find((e: IProductProps) => e.id === product.id)
+
+  //   if (productInCart) {
+  //     const newData = data.find((e: IProductProps) => e.id !== product.id)
+  //     setData(newData)
+  //     setItem({ key: 'shopCart', value: newData })
+
+  //     const newQuantity = { ...quantity }
+  //     delete newQuantity[product.id]
+  //     setQuantity(newQuantity)
+  //     setItem({ key: 'quantity', value: newQuantity })
+
+  //     const event = new CustomEvent('cartChange', { detail: newData })
+  //     window.dispatchEvent(event)
+  //   } else {
+  //     setData([...data, product])
+  //     setItem({ key: 'shopCart', value: [...data, product] })
+
+  //     const newQuantity = { ...quantity, [product.id]: 1 }
+  //     setQuantity(newQuantity)
+  //     setItem({ key: 'quantity', value: newQuantity })
+
+  //     const event = new CustomEvent('cartChange', {
+  //       detail: [...data, product],
+  //     })
+  //     window.dispatchEvent(event)
+  //   }
+  // }
 
   const removeProduct = (id: string) => {
     if (quantity[id] === 1) {
@@ -85,7 +110,7 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
   }
 
   return (
-    <CartContext.Provider value={{ data, quantity, addProduct, removeProduct }}>
+    <CartContext.Provider value={{ data, addProduct, quantity, removeProduct }}>
       {children}
     </CartContext.Provider>
   )
