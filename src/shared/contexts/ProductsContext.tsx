173a993e1e03da3345be79ec440024b78498117e
Davi Alves <cadwvk@gmail.com>
2023-06-29 16:28:30 -0300

    added fetch api in Products context

--- src/shared/contexts/ProductsContext.tsx
+++ src/shared/contexts/ProductsContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { apiBaseUrl } from '../api/api'
 
 interface IProductProps {
   id: string
@@ -23,5 +24,16 @@ export const ProductsContext = createContext<IProductsContext>(
 export const ProductsProvider: React.FC<ProductsProviderProps> = ({
   children,
 }) => {
+  const [products, setProducts] = useState<IProductProps[]>([])
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch(apiBaseUrl)
+      const objJson = await response.json()
+      setProducts(objJson.results)
+    }
+    fetchApi()
+  }, [])
+
   return <>{children}</>
 }
