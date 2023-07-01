cfce99d92f06ac2808cd001d075045abf9b53001
Davi Alves <cadwvk@gmail.com>
2023-07-01 14:45:53 -0300

    Added title in faq page

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@/shared/components/container/Container'
+import { THeadingPrimary } from '@/shared/fonts/Fonts.style'
 import { LayoutBaseDePagina } from '@/shared/layout'
 
 export function Faq() {
@@ -11,7 +12,9 @@ export function Faq() {
         flexDir="column"
         width="100%"
         height="100%"
-      ></Container>
+      >
+        <THeadingPrimary>FAQ - Perguntas Frequentes</THeadingPrimary>
+      </Container>
     </LayoutBaseDePagina>
   )
 }
