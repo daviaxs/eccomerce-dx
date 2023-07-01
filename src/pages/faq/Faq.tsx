5c27457d4846b812c10664266c6361d8d083f106
Davi Alves <cadwvk@gmail.com>
2023-07-01 14:45:13 -0300

    Added layoutBaseDePagina in faq

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -1,3 +1,17 @@
+import { Container } from '@/shared/components/container/Container'
+import { LayoutBaseDePagina } from '@/shared/layout'
+
 export function Faq() {
-  return <></>
+  return (
+    <LayoutBaseDePagina>
+      <Container
+        display="flex"
+        align="center"
+        justifyContent="start"
+        flexDir="column"
+        width="100%"
+        height="100%"
+      ></Container>
+    </LayoutBaseDePagina>
+  )
 }
