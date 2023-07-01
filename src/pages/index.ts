bd5640bcf4a61c88d6ef952ae95e1d2ad0bc65a3
Davi Alves <cadwvk@gmail.com>
2023-07-01 14:43:23 -0300

    Created faq route

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export * from '@/pages/home/Home'
+export * from '@/pages/faq/Faq'
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { Home } from '@/pages'
+import { Faq, Home } from '@/pages'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Home />} />
+      <Route path="/faq" element={<Faq />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
