1663c70d365450b29778644769068725053acc02
Davi Alves <cadwvk@gmail.com>
2023-07-01 13:37:29 -0300

    Added verification in ButtonPrimary on CartFooter

--- src/shared/components/cartMenu/utils/CartFooter.tsx
+++ src/shared/components/cartMenu/utils/CartFooter.tsx
@@ -53,7 +53,7 @@ export function CartFooter() {
         width="90%"
         onClick={() => alert('hello world')}
         className="button"
-        disabled
+        disabled={!(data.length > 0)}
       >
         <THeadingPrimary txtColor="white" fontSize={1.5}>
           Concluir compra
